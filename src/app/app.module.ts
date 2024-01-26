96208e163de4d47912c5acb83aa82df8ad7ed037
Shimon S <shimonslit@gmail.com>
2024-01-26 10:01:26 +0100

    reloadfile-css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CocktailService } from './cocktail.service';
 
 @NgModule({
   declarations: [
+    CocktailListComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CocktailService],
   bootstrap: [AppComponent]
+   
 })
 export class AppModule { }
